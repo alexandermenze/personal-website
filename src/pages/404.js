025ea52167b6674188b752e21bc73132a0574ff2
Alexander Menze <a.menze@polipol.de>
2021-03-17 21:41:28 +0100

    Initial commit

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,11 @@
-import * as React from "react"
-import { graphql } from "gatsby"
+import React from "react"
+import * as styles from "./404.module.css"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
-  )
-}
-
-export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
+export default () => (
+    <div>
+        <h1 className={styles.header}>404 - Page not found</h1>
+        <p className={styles.errorMessage}>
+            Lorem ipsum 404
+        </p>
+    </div>
+)
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
-import * as styles from "./index.module.css";
 
 export default () => (
     <div>
-        <h1 className={styles.test}>This is the index page</h1>
+        <h1>This is the about page</h1>
         <div>
             <Link to="/">Home</Link> | <Link to="/about">About</Link>
         </div>
         <p>
-            Lorem ipsum
+            Lorem ipsum about
         </p>
     </div>
 )

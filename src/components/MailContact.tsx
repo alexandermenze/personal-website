a7b214c91fafb07b3d44ecdd400b888142245cd5
Alexander Menze <a.menze@polipol.de>
2021-06-07 22:02:17 +0200

    Finished mail contact

--- src/components/MailContact.tsx
+++ src/components/MailContact.tsx
@@ -1,12 +1,37 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
-import { widths, margins, colors } from '../styles/variables'
+import { widths, margins } from '../styles/variables'
+import VerticalLine from './VerticalLine'
 
-const StyledLine = styled.div`
-  width: 1px;
-  height: 100px;
-  background: black;
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: ${margins.default}px;
+const StyledMailContact = styled.div`
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  width: ${widths.sideBar}px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `
+
+const StyledMailAdress = styled.p`
+  position: relative;
+  margin-top: ${margins.defaultHalf}px;
+  margin-bottom: ${margins.defaultHalf}px;
+  transition: all 0.2s;
+  &:hover {
+    transform: translateY(-${margins.defaultHalf}px);
+  }
+`
+
+interface MailContactProps {
+  mailAddress: string
+}
+
+const MailContact: React.FC<MailContactProps> = ({ mailAddress }) => (
+  <StyledMailContact>
+    <StyledMailAdress>{mailAddress}</StyledMailAdress>
+    <VerticalLine />
+  </StyledMailContact>
+)
+
+export default MailContact

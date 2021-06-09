97a738ca1ffcac1fd7a6327ec14ddf8a24a89109
Alexander Menze <a.menze@polipol.de>
2021-06-09 23:48:41 +0200

    Added image component

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,6 +67,7 @@ module.exports = {
     },
     'gatsby-plugin-emotion',
     'gatsby-plugin-typescript',
+    'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet'
--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
-import { keyframes } from '@emotion/core'
+import { css, keyframes } from '@emotion/core'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const opacityAnimation = keyframes`
   0%   { opacity: 0; }
@@ -15,6 +16,12 @@ const Container = styled.div`
   width: auto;
   max-width: 100em;
   height: 100vh;
+`
+
+const InnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
 
 const NameHeader = styled.h1`
@@ -40,17 +47,40 @@ const LongDescriptionText = styled.p`
   animation: 1.5s ease-out 0s 1 ${opacityAnimation};
 `
 
+const ProfileImageContainer = styled.div`
+  min-width: 300px;
+  margin-left: 50px;
+`
+
+const RoundedCorners = css`
+  border-radius: 25px;
+`
+
 interface PersonalInfoProps {
   name: string
   shortDescription: string
   longDescription: string
+  imagePath: string
 }
 
-const PersonalInfo: React.FC<PersonalInfoProps> = ({ name, shortDescription, longDescription }) => (
+const PersonalInfo: React.FC<PersonalInfoProps> = ({ name, shortDescription, longDescription, imagePath }) => (
   <Container>
-    <NameHeader>{name}</NameHeader>
-    <ShortDescriptionText>{shortDescription}</ShortDescriptionText>
-    <LongDescriptionText>{longDescription}</LongDescriptionText>
+    <InnerContainer>
+      <div>
+        <NameHeader>{name}</NameHeader>
+        <ShortDescriptionText>{shortDescription}</ShortDescriptionText>
+        <LongDescriptionText>{longDescription}</LongDescriptionText>
+      </div>
+      <ProfileImageContainer>
+        <StaticImage
+          imgClassName={`${RoundedCorners}`}
+          src="../assets/images/profile_image.jpg"
+          alt="Profile image"
+          width={300}
+          height={300}
+        />
+      </ProfileImageContainer>
+    </InnerContainer>
   </Container>
 )
 

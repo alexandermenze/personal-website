fef3fadb16482f57b5963cb37354cf7ebff742cc
Alexander Menze <a.menze@polipol.de>
2021-08-01 18:00:36 +0200

    Fixed positioning of resume

--- src/components/PageContentContainer.tsx
+++ src/components/PageContentContainer.tsx
@@ -5,21 +5,26 @@ import { breakpoints } from '../styles/variables'
 const BreakpointContainer = styled.div`
   position: relative;
   display: block;
+  margin: 0 auto;
 
   @media (min-width: ${breakpoints.sm}px) {
     max-width: 450px;
+    width: 450px;
   }
 
   @media (min-width: ${breakpoints.md}px) {
     max-width: 550px;
+    width: 550px;
   }
 
   @media (min-width: ${breakpoints.lg}px) {
     max-width: 730px;
+    width: 730px;
   }
 
   @media (min-width: ${breakpoints.xl}px) {
     max-width: 900px;
+    width: 900px;
   }
 `
 
